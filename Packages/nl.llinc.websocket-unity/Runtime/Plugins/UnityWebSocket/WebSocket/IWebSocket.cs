0ff6e7a9ba6ca63c4b1afff27a9df1f2c92f846a
Adel Qaddoumi <adel.qadd@gmail.com>
2023-07-19 11:48:36 +0200

    added plugins

---
+++ Packages/nl.llinc.websocket-unity/Runtime/Plugins/UnityWebSocket/WebSocket/IWebSocket.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Unity3dAzure.WebSockets {
+  public interface IWebSocket {
+    void ConfigureWebSocket(string url);
+    void ConnectAsync();
+    void CloseAsync();
+
+    void SendAsync(byte[] data, Action<bool> completed = null);
+    void SendAsync(string text, Action<bool> completed = null);
+
+    bool IsConfigured();
+    bool IsOpen();
+    string Url();
+
+    event OnError OnError;
+    event OnOpen OnOpen;
+    event OnMessage OnMessage;
+    event OnClose OnClose;
+  }
+
+  public delegate void OnError(object sender, WebSocketErrorEventArgs e);
+  public delegate void OnOpen(object sender, EventArgs e);
+  public delegate void OnMessage(object sender, WebSocketMessageEventArgs e);
+  public delegate void OnClose(object sender, WebSocketCloseEventArgs e);
+}
